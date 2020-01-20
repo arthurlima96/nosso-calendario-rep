90f1edaa587970a1a853c325c21886304eeaec0c
Arthur C. Lima <arthur.lima@sefaz.ma.gov.br>
2020-01-20 09:06:28 -0300

    Configurando Setup

---
+++ nosso-calendario/src/main/java/com/br/devefc/nossocalendario/NossoCalendarioApplication.java
@@ -0,0 +1,13 @@
+package com.br.devefc.nossocalendario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NossoCalendarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NossoCalendarioApplication.class, args);
+	}
+
+}
