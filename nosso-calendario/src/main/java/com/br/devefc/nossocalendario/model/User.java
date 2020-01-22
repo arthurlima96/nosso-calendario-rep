864c83bb35d7d12aa1534ef3dc824cf4528ead6c
arthurlima96 <arthurcantanhede@hotmail.com>
2020-01-22 02:07:07 -0300

    Aplicação do desafio da senha com o hash e melhorias sugeridas no modelo

--- nosso-calendario/src/main/java/com/br/devefc/nossocalendario/model/User.java
+++ nosso-calendario/src/main/java/com/br/devefc/nossocalendario/model/User.java
@@ -12,59 +12,62 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
 	@NotBlank(message = "Login Obrigatorio")
 	@NotEmpty(message = "Login não pode ser vazio")
 	@Email(message = "Email deve ser valido")
 	private String login;
-	
+
 	@NotBlank(message = "Senha Obrigatoria")
 	@NotEmpty(message = "Senha não pode ser vazia")
 	@Size(min = 6, message = "A senha deve ter no minimo 6 caracteres")
 	private String senha;
-	
+
 	@CreationTimestamp
 	private LocalDateTime acesso_cadastro;
-	
+
 	public User() {
 	}
-	
-	public User(
-			@NotBlank
-			@NotEmpty
-			@Email String login, 
-			@NotBlank
-			@NotEmpty
-			@Size(min = 6)String senha) {
+
+	public User(@NotBlank @NotEmpty @Email String login, @NotBlank @NotEmpty @Size(min = 6) String senha) {
 		this.login = login;
-		this.senha = senha;
+		this.senha = this.encodeSenha(senha);
+	}
+
+	private String encodeSenha(String senha) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+		String encodedSenha = encoder.encode(senha);
+		return encodedSenha;
 	}
 
 	public Long getId() {
 		return id;
 	}
-	public void setId(Long id) {
-		this.id = id;
-	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getSenha() {
 		return senha;
 	}
+
 	public void setSenha(String senha) {
-		this.senha = senha;
+		this.senha = this.encodeSenha(senha);
 	}
+
 	public LocalDateTime getAcesso_cadastro() {
 		return acesso_cadastro;
 	}
