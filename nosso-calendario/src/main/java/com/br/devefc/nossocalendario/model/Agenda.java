8b61d96e10128237db130f337ae536437c1594ca
arthurlima96 <arthurcantanhede@hotmail.com>
2020-01-22 19:31:49 -0300

    Criação modelo Agenda

---
+++ nosso-calendario/src/main/java/com/br/devefc/nossocalendario/model/Agenda.java
@@ -0,0 +1,58 @@
+package com.br.devefc.nossocalendario.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Agenda {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@NotNull
+	@NotEmpty
+	private String nome;
+	
+	@NotNull
+	@NotEmpty
+	@Size(max = 255)
+	private String descricao;
+	
+	@NotNull
+	private User dono;
+	
+	public Agenda() {
+	}
+
+	public Agenda(@NotNull @NotEmpty String nome, @NotNull @NotEmpty @Size(max = 255) String descricao,
+			@NotNull User dono) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.dono = dono;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public User getDono() {
+		return dono;
+	}
+	
+	
+}
