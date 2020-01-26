ce9a69d427e4d1e9b57374682315946970baa751
Arthur Lima <arthurcantanhede@hotmail.com>
2020-01-26 12:52:55 -0300

    Liberação dos acessos ao h2-console e swagger

--- nosso-calendario/src/main/java/com/br/devefc/nossocalendario/security/WebSecurityConfig.java
+++ nosso-calendario/src/main/java/com/br/devefc/nossocalendario/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UsersService usersService;
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/**.html",  "/v2/api-docs", "/webjars/**", 
+				"/configuration/**", "/swagger-resources/**","/h2-console/**", "/css/**", "/**.ico", "/js/**");
+	}
+
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
